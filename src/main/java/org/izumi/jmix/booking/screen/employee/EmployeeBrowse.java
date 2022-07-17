fadf644368a4bad730815f5b28d5b30c96bc6d6a
Aiden <a.izumi@haulmont.com>
2022-07-17 23:15:50 +0400

    Add employee screens

---
+++ src/main/java/org/izumi/jmix/booking/screen/employee/EmployeeBrowse.java
@@ -0,0 +1,13 @@
+package org.izumi.jmix.booking.screen.employee;
+
+import io.jmix.ui.screen.LookupComponent;
+import io.jmix.ui.screen.StandardLookup;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+import org.izumi.jmix.booking.entity.user.Employee;
+
+@UiController("Employee.browse")
+@UiDescriptor("employee-browse.xml")
+@LookupComponent("employeesTable")
+public class EmployeeBrowse extends StandardLookup<Employee> {
+}
